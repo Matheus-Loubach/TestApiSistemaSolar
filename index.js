e5dec9ffa4e2a2a66668d7464e3f2530961af44f
matheus <matheus.loubach@gmail.com>
2022-11-07 20:28:41 -0300

    teste2

--- index.js
+++ index.js
@@ -8,9 +8,49 @@ const PORT = process.env.PORT || 8877;
 
 app.get('/planetas',(req, res) =>{
   res.json({
-    planeta: [
+    planeta:
+    [ 
       {
-        id: 1,
+      id: 1,
+      name: 'Venus',
+      desc: 'Marte é um mundo empoeirado, frio e desértico com uma atmosfera muito fina. Há fortes evidências de que Marte era – bilhões de anos atrás – mais úmido e quente, com uma atmosfera mais espessa.',
+      diametro:6792.4,
+      massa:'6,4174×1023',
+      distancia:'227.943.824',
+      rotacao:24,
+      translacao:686.971,
+      temperatura:63,
+      atmosfera:'Dióxido de Carbono,Argônio,Nitrogênio,Oxigênio,Monóxido de carbono',
+      img: 'https://solarsystem.nasa.gov/system/stellar_items/list_view_images/2_mercury_480x320_new.jpg'
+    },
+    {
+        id: 2,
+        name: 'Venus',
+        desc: 'Marte é um mundo empoeirado, frio e desértico com uma atmosfera muito fina. Há fortes evidências de que Marte era – bilhões de anos atrás – mais úmido e quente, com uma atmosfera mais espessa.',
+        diametro:6792.4,
+        massa:'6,4174×1023',
+        distancia:'227.943.824',
+        rotacao:24,
+        translacao:686.971,
+        temperatura:63,
+        atmosfera:'Dióxido de Carbono,Argônio,Nitrogênio,Oxigênio,Monóxido de carbono',
+        img: 'https://solarsystem.nasa.gov/system/stellar_items/list_view_images/3_480x320_venus.png'
+      },
+      {
+        id: 3,
+        name: 'terra',
+        desc: 'Marte é um mundo empoeirado, frio e desértico com uma atmosfera muito fina. Há fortes evidências de que Marte era – bilhões de anos atrás – mais úmido e quente, com uma atmosfera mais espessa.',
+        diametro:6792.4,
+        massa:'6,4174×1023',
+        distancia:'227.943.824',
+        rotacao:24,
+        translacao:686.971,
+        temperatura:63,
+        atmosfera:'Dióxido de Carbono,Argônio,Nitrogênio,Oxigênio,Monóxido de carbono',
+        img: 'https://solarsystem.nasa.gov/system/stellar_items/list_view_images/4_earth_480x320.jpg'
+      },
+      {
+        id: 4,
         name: 'Marte',
         desc: 'Marte é um mundo empoeirado, frio e desértico com uma atmosfera muito fina. Há fortes evidências de que Marte era – bilhões de anos atrás – mais úmido e quente, com uma atmosfera mais espessa.',
         diametro:6792.4,
@@ -22,19 +62,7 @@ app.get('/planetas',(req, res) =>{
         atmosfera:'Dióxido de Carbono,Argônio,Nitrogênio,Oxigênio,Monóxido de carbono',
         img: 'https://solarsystem.nasa.gov/system/stellar_items/list_view_images/6_mars_480x320.jpg'
       },
-      {
-        id: 2,
-        name: 'Venus',
-        desc: 'Marte é um mundo empoeirado, frio e desértico com uma atmosfera muito fina. Há fortes evidências de que Marte era – bilhões de anos atrás – mais úmido e quente, com uma atmosfera mais espessa.',
-        diametro:6792.4,
-        massa:'6,4174×1023',
-        distancia:'227.943.824',
-        rotacao:24,
-        translacao:686.971,
-        temperatura:63,
-        atmosfera:'Dióxido de Carbono,Argônio,Nitrogênio,Oxigênio,Monóxido de carbono',
-        img: 'https://solarsystem.nasa.gov/system/stellar_items/list_view_images/6_mars_480x320.jpg'
-      },
+
     ]
   })
 })
